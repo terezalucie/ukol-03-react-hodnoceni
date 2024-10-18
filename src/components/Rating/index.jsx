d4d90c3904c8c659664d98390d3caf8b242efe6c
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-10-18 15:35:15 +0200

    verze01

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -9,11 +9,11 @@ export const Rating = () => {
     <div className="rating">
       <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <Star glowing={rating >= 1} onSelect={setRating} value={1} />
+        <Star glowing={rating >= 2} onSelect={setRating} value={2} />
+        <Star glowing={rating >= 3} onSelect={setRating} value={3} />
+        <Star glowing={rating >= 4} onSelect={setRating} value={4} />
+        <Star glowing={rating === 5} onSelect={setRating} value={5} />
       </div>
     </div>
   );
